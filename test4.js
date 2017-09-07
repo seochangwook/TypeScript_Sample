a9aa338507d25dc937cf487d36f9e81917621ae7
seochangwook <scw05313315@gmail.com>
2017-09-07 18:12:50 +0900

    interface

---
+++ test4.js
@@ -0,0 +1,6 @@
+//인터페이스//
+function printLabel(labelledObj) {
+    console.log(labelledObj.label);
+}
+var myObj = { label: '10' };
+printLabel(myObj);
---
+++ test4.ts
@@ -0,0 +1,7 @@
+//인터페이스//
+function printLabel(labelledObj:{label:string}){
+    console.log(labelledObj.label);
+}
+
+var myObj = {label:'10'};
+printLabel(myObj);
