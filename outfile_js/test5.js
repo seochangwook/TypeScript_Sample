d92cb459ed44fd9a8060a936c33848ff13bbea0b
seochangwook <scw05313315@gmail.com>
2017-09-08 14:09:08 +0900

    enum, any, void, null, undefined, Type Assertions

---
+++ outfile_js/test5.js
@@ -0,0 +1,31 @@
+//enum//
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
+})(Color || (Color = {}));
+;
+var color_name = Color[2];
+console.log(color_name);
+//Any type : 데이터 타입을 미리 알 수 없을 때 사용//
+var anyvalue;
+anyvalue = 10;
+console.log('anyvalue(num): ' + anyvalue);
+anyvalue = 'seo';
+console.log('anyvalue(string): ' + anyvalue);
+//Void type : any의 반대개념으로 어떤 타입도 가질 수 없을 경우//
+function voidtest() {
+    console.log('void function message...');
+    //return '10';
+}
+voidtest();
+//Null and Undefined//
+var nullvalue = null;
+var undefinevalue = undefined;
+console.log('nullvalue: ' + nullvalue);
+console.log('undefined value: ' + undefinevalue);
+//Type Assertions//
+var original_value = 'seochangwook';
+var ori_str_value_length = original_value.length;
+console.log('str size: ' + ori_str_value_length);
---
+++ sample_ts/test5.ts
@@ -0,0 +1,33 @@
+//enum//
+enum Color {Red = 1, Green, Blue};
+var color_name:string = Color[2];
+
+console.log(color_name);
+
+//Any type : 데이터 타입을 미리 알 수 없을 때 사용//
+var anyvalue:any;
+anyvalue = 10;
+console.log('anyvalue(num): ' + anyvalue);
+anyvalue = 'seo';
+console.log('anyvalue(string): ' + anyvalue);
+
+//Void type : any의 반대개념으로 어떤 타입도 가질 수 없을 경우//
+function voidtest() : void{
+    console.log('void function message...');
+
+    //return '10';
+}
+
+voidtest();
+
+//Null and Undefined//
+var nullvalue : null = null;
+var undefinevalue : undefined = undefined;
+
+console.log('nullvalue: ' + nullvalue);
+console.log('undefined value: ' + undefinevalue);
+
+//Type Assertions//
+var original_value:any = 'seochangwook';
+var ori_str_value_length:number = (<string>original_value).length;
+console.log('str size: ' + ori_str_value_length);
